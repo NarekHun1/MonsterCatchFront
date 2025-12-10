33acfafc65b56c60bd6a8b1c7adee48232c1ab25
narek <narekhunanyan0497@gmail.com>
2025-12-10 17:03:07 +0400

    add buffer

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/',              // ← ОБЯЗАТЕЛЬНО ДЛЯ TELEGRAM WEBAPP
-    build: {
-        outDir: 'dist'
+    resolve: {
+        alias: {
+            buffer: 'buffer',
+        },
     },
-    server: {
-        host: true
+    optimizeDeps: {
+        esbuildOptions: {
+            define: {
+                global: 'globalThis',
+            },
+            plugins: [
+                NodeGlobalsPolyfillPlugin({
+                    buffer: true,
+                }),
+                NodeModulesPolyfillPlugin()
+            ],
+        },
     },
-    preview: {
-        host: true
-    }
 });
