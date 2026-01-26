d46c9e081b2bd91692c73ca9a7e2c25cc5a100ff
narek <narekhunanyan0497@gmail.com>
2026-01-26 13:09:54 +0400

    add match 3 game

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,8 @@
     import type { Lang } from './i18n';
     import {Quests} from "./Quests.tsx";
     import { BottomNav } from './BottomNav';
-    import LevelsMap from './pages/LevelsMap.tsx';
-    import Match3Level from './pages/Match3Level.tsx';
+    import LevelsMap from './LevelsMap.tsx';
+    import Match3Level from './Match3Level.tsx';
 
 
 
